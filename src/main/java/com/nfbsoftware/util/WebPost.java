d847a717fd275c5d94326fde36cfffc3ec1ec8eb
bclemenzi <brendan@clemenzi.com>
2015-12-22 11:41:25 -0500

    Fixed a problem with https sites.

--- src/main/java/com/nfbsoftware/util/WebPost.java
+++ src/main/java/com/nfbsoftware/util/WebPost.java
@@ -1,9 +1,7 @@
 package com.nfbsoftware.util;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
@@ -76,7 +74,7 @@ public class WebPost
                 m_outputStream = m_httpUrlConnection.getOutputStream();
             }
             
-            m_outputStream.write(sRequest.getBytes("UTF-8"));
+            m_outputStream.write(sRequest.getBytes());
             m_outputStream.flush();
         }
         catch(IOException e)
@@ -99,7 +97,7 @@ public class WebPost
                 m_outputStream = m_httpsUrlConnection.getOutputStream();
             }
             
-            m_outputStream.write(sRequest.getBytes("UTF-8"));
+            m_outputStream.write(sRequest.getBytes());
             m_outputStream.flush();
         }
         catch(IOException e)
@@ -368,21 +366,21 @@ public class WebPost
     {
         try
         {
-            StringBuffer responseBuffer = new StringBuffer(500);
+            StringBuffer responseBuffer = new StringBuffer (500);
             
             if (m_inputStream == null)
             {
                 m_inputStream = m_httpUrlConnection.getInputStream();
             }
             
-            BufferedReader in = new BufferedReader(new InputStreamReader(m_inputStream, "UTF-8"));
-            String readLine;
-
-            while ((readLine = in.readLine()) != null) 
+            int kar; 
+            char ch;
+            while ( (kar = m_inputStream.read () ) != -1 )
             {
-                responseBuffer.append(readLine);
+              ch = (char) kar; responseBuffer.append(ch);
             }
-
+            m_inputStream.close();
+            
             return new String(responseBuffer);
         }
         catch(IOException e)
@@ -395,21 +393,21 @@ public class WebPost
     {
         try
         {
-            StringBuffer responseBuffer = new StringBuffer(500);
+            StringBuffer responseBuffer = new StringBuffer (500);
             
             if (m_inputStream == null)
             {
                 m_inputStream = m_httpsUrlConnection.getInputStream();
             }
             
-            BufferedReader in = new BufferedReader(new InputStreamReader(m_inputStream, "UTF-8"));
-            String readLine;
-
-            while ((readLine = in.readLine()) != null) 
+            int kar; 
+            char ch;
+            while ( (kar = m_inputStream.read () ) != -1 )
             {
-                responseBuffer.append(readLine);
+              ch = (char) kar; responseBuffer.append(ch);
             }
-
+            m_inputStream.close();
+            
             return new String(responseBuffer);
         }
         catch(IOException e)
